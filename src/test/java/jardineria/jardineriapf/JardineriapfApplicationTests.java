1504964a1afc92f825704f3ebf5d83dc43e1a652
IvoMart <ivo_ivancho@hotmail.com>
2022-12-07 21:02:49 -0300

    Spring boot - init

---
+++ src/test/java/jardineria/jardineriapf/JardineriapfApplicationTests.java
@@ -0,0 +1,13 @@
+package jardineria.jardineriapf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JardineriapfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
