5e14d9b45ba6c7755201d69ecd42f05e517e2d6c
L0RDZ3R0 <romeroarmando094@gmail.com>
2022-12-24 06:51:05 -0300

    Servicio de Email

--- src/main/java/jardineria/jardineriapf/repositorios/UsuarioRepositorio.java
+++ src/main/java/jardineria/jardineriapf/repositorios/UsuarioRepositorio.java
@@ -1,5 +1,5 @@
 package jardineria.jardineriapf.repositorios;
-// contraseña de spring --> edlx cjrp haqg ecik
+// contraseña de spring --> vfzbzhbnggrurkur 
 import jardineria.jardineriapf.entidades.*;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
